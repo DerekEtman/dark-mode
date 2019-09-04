ba1d5008430a8b83132eea2616c13ab3bc458b70
Derek Etman <Dereketman@gmail.com>
2019-09-04 13:13:09 -0700

    Staging all changes

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+    const item = JSON.parse(localStorage.getItem(key));
+
+    const [storedValue, setStoredValue] = useState(item || initialValue);
+
+    const setValue = value => {
+        if (localStorage.getItem(key) === null)
+            {
+            localStorage.setItem(key, JSON.stringify(value));
+            setStoredValue(value);
+            }
+    };
+
+    return [storedValue, setValue]
+}
