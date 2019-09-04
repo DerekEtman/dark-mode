52f12f7aeb82bdfc56326f9b4cecc0df8946fc09
Derek Etman <Dereketman@gmail.com>
2019-09-04 13:18:46 -0700

    Rewrote using Readme Instructions.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,17 +1,15 @@
 import { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
-    const item = JSON.parse(localStorage.getItem(key));
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
 
-    const [storedValue, setStoredValue] = useState(item || initialValue);
+    const setValue = (value) =>{
+        setStoredValue(value);
+        localStorage.setItem(key, JSON.stringify(value));
+    }
 
-    const setValue = value => {
-        if (localStorage.getItem(key) === null)
-            {
-            localStorage.setItem(key, JSON.stringify(value));
-            setStoredValue(value);
-            }
-    };
-
-    return [storedValue, setValue]
-}
+    return [storedValue, setStoredValue]
+};
